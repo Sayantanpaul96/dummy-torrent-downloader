f938f1d2f9e9b63c50ced8a66c9abdf50a353b51
Sayantan paul <paulsayantan96@gmail.com>
2024-11-02 18:10:57 +0530

    new repo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cliProgress from 'cli-progress';
+import chalk from 'chalk';
 
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 const __filename = fileURLToPath(import.meta.url);
@@ -10,24 +11,27 @@ const __dirname = path.dirname(__filename);
 
 const torrentID = process.argv[2]; // get the torrent ID from the command line
 const customPath = process.argv[3]; // create a folder name or a custom path
-console.log("Torrent ID: ", torrentID);
+console.log(chalk.yellow('Recieved Torrent link or location: '), torrentID);
 
 const client = new WebTorrent();
 
 // Specify the folder to save files
 const outputDir = path.isAbsolute(customPath) ? customPath : path.join(__dirname, customPath);
+console.log(chalk.yellow('creating in Path: ', outputDir));
 
 // Create the folder if it doesn't exist
 if (!fs.existsSync(outputDir, { recursive: true })) {
+    console.log(chalk.yellow('created: ', outputDir));
     fs.mkdirSync(outputDir);
 }
 
 try {
+    console.log(chalk.greenBright('Starting torrent download...'));
     client.add(torrentID, torrent => {
         const files = torrent.files;
         let length = files.length;
 
-        console.log("Number of files: ", length);
+        console.info(chalk.green("Number of files to be downloaded: "), length);
         bar.start(100, 0)
         let interval = setInterval(() => {
             // console.log("Progress: ", (torrent.progress * 100).toFixed(), "%")
@@ -40,11 +44,11 @@ try {
             const destination = fs.createWriteStream(destinationPath);
 
             source.on('error', err => {
-                console.error(`Error reading file ${file.name}:`, err);
+                console.error(chalk.redBright(`Error reading file ${file.name}:\t`), err);
             });
 
             destination.on('error', err => {
-                console.error(`Error writing file ${file.name}:`, err);
+                console.error(chalk.redBright(`Error writing file ${file.name}:\t`), err);
             });
 
             destination.on('finish', () => {
@@ -53,7 +57,7 @@ try {
                     bar.update(100);
                     bar.stop();
                     clearInterval(interval);
-                    console.log("\n All files processed. Exiting...");
+                    console.log(chalk.greenBright("\n All files processed. Exiting..."));
                     process.exit();
                 }
             });
@@ -62,6 +66,6 @@ try {
         });
     });
 } catch (err) {
-    console.error(`Error adding torrent: ${err.message}`);
+    console.error(chalk.redBright('Error adding torrent:\t',`${err.message}`));
     process.exit(1);
 }
